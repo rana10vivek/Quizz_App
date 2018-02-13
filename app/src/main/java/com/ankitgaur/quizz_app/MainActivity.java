a85ab3c46942e7a1059d0de35bd226e2125afab7
ankit gaur <ankit.gaur1023@gmail.com>
2018-02-13 23:08:02 +0530

    Initial commit

---
+++ app/src/main/java/com/ankitgaur/quizz_app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ankitgaur.quizz_app;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button mstart_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
